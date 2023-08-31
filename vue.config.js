1a08f11425f1c60e6c351c1bcc62d74fc7146762
Orikson <42471346+Orikson@users.noreply.github.com>
2023-08-30 18:19:28 -0700

    Update public path for production deployment

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/bomb_calorimetry_v2/" : "/",
 
   pluginOptions: {
     vuetify: {
